6f341442476cda6a53fa8a4792ea4db8b40a0ea7
maish79 <maishsupa@yahoo.com>
2022-10-15 14:24:58 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,124 @@
-//wait for DOM to finish loading before running the game
-//Get the button elements and add event listeners to them
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded",function() {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type")==="submit") {
+            if (this.getAttribute("data-type") === "submit") {
                 checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-
+                runGame(gameType);
             }
-        })
+        });
     }
 
     runGame("addition");
-})
+
+});
+
 /**
- * The main game "loop",called when the script is first loaded
+ * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(gameType){
-// Creates two random numbers between 1 and 25
-let num1 = Math.floor(Math.random() *25) +1;
-let num2 = Math.floor(Math.random() *25) +1;
+function runGame(gameType) {
 
-if (gameType === "addition") {
-    displayAdditionQuestion(num1, num2);
-} else {
-    alert(`unknown game type: ${gameType}`);
-    throw `unknown game type: ${gametype}. Aborting!`;
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
-}
+
 /**
- * Checks the answer against the first element in 
- * the returned calculatedCorrectAnswer array
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
  */
-function checkAnswer(){
-let userAnswer = parseInt(document.getElementById("answer-box").value)
-let calculatedAnswer = calculateCorrectAnswer();
-let iscorrect = userAnswer === calculatedAnswer[0];
+function checkAnswer() {
 
-if (iscorrect) {
-    alert("Hey!You got it right! :D");
-    incrementScore()
-     } else {
-        alert(`Awww... you answered ${userAnswer}.The correct answer was ${calculatedAnswer[0]}!`);
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+        incrementScore();
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
         incrementWrongAnswer();
-     }
-  runGame(calculatedAnswer[1])
+    }
 
+    runGame(calculatedAnswer[1]);
 
 }
+
 /**
- * Gets the operand (the numbers) and the operator (plus ,minus etc)
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
  * directly from the dom, and returns the correct answer.
  */
-function calculateCorrectAnswer(){
+function calculateCorrectAnswer() {
 
-let operand1 =parseInt(document.getElementById('operand1').innerText);
-let operand2 =parseInt(document.getElementById('operand2').innerText);
-let operator=document.getElementById("operator").innerText;
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
 
-if(operator === "+") {
-    return [operand1 + operand2, "addition"];
-} else {
-    alert(`Unimplemented operator ${operator}`);
-    throw `Unimplemented operator ${operator}.Aborting!`;
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+
 }
 
-}
 /**
- * gets the current score from the Dom and increments it by 1
+ * Gets the current score from the DOM and increments it by 1
  */
+function incrementScore() {
 
-function incrementScore(){
- 
-    let oldScore= parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText= ++oldScore;
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
-function incrementWrongAnswer(){
 
-    let oldScore= parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText= ++oldScore;
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
 }
-function displayAdditionQuestion(operand1, operand2){
+
+function displayAdditionQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
-
-  
+    
 }
-function displayMultiplyQuestion(){
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
